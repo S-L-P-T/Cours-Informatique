e22f16399c71f49ed45dcf1671101b6ad7ab5e49
S-L-P-T <81326242+S-L-P-T@users.noreply.github.com>
2023-01-04 15:24:12 +0100

    Update Syracuse.py

--- Unix/Syracuse.py
+++ Unix/Syracuse.py
@@ -1,6 +1,17 @@
 def syracuse(n):
     a = 0
+    b = n
+    altimax = b
+    altimin = b
+    altimoy = b
+    d = 0
+    dureealtitudemax = 0
+    dureealtitudemin = 0
+    dureealtitudemoy = 0
+    nbduree = 0
     if n == 0:
+        print("non")
+    elif n <= 0:
         print("non")
     else:
         print(a," ",n)
@@ -9,9 +20,55 @@ def syracuse(n):
                 n = n//2
                 a = a+1
                 print(a," ",n)
+                if altimax < n:
+                    altimax = n
+                if altimin > n:
+                    altimin = n
+                altimoy = altimoy + n
+                if b < n:
+                    d = d+1
+                else:
+                    if d > dureealtitudemax:
+                        dureealtitudemax = d
+                    elif dureealtitudemin > dureealtitudemax:
+                        dureealtitudemin = dureealtitudemax
+                    dureealtitudemoy = dureealtitudemoy + d
+                    if d != 0:
+                        nbduree = nbduree + 1
+                    d = 0
+                    d = 0
             else:
                 n = (n*3)+1
                 a = a+1
                 print(a," ",n)
+                if altimax < n:
+                    altimax = n
+                if altimin > n:
+                    altimin = n
+                altimoy = altimoy + n
+                if b < n:
+                    d = d+1
+                else:
+                    if d > dureealtitudemax:
+                        dureealtitudemax = d
+                    elif dureealtitudemin > dureealtitudemax:
+                        dureealtitudemin = dureealtitudemax
+                    dureealtitudemoy = dureealtitudemoy + d
+                    if d != 0:
+                        nbduree = nbduree + 1
+                    d = 0
 
-syracuse(20)
+        altimoy = altimoy//a
+        dureealtitudemoy = dureealtitudemoy//nbduree
+
+        print("Durée de vol maximale = ",a)
+        print("Durée de vol minimale = ",a)
+        print("Durée de vol moyen = ",a//a)
+        print("Altitude maximale = ",altimax)
+        print("Altitude minimale = ",altimin)
+        print("Altitude moyenne = ",altimoy)
+        print("Durée de vol maximale en altitude = ",dureealtitudemax)
+        print("Durée de vol minimale en altitude = ",dureealtitudemin)
+        print("Durée de vol moyen en altitude = ",dureealtitudemoy)
+
+syracuse(11)
